51191169edfa31aa963a7d112a282b99bea313e0
Felipebros <felipeiturama@gmail.com>
2021-11-23 22:35:33 -0300

    feat: editar não finalizado

--- app/obras.py
+++ app/obras.py
@@ -64,18 +64,45 @@ def listar():
 
 
 @bp_obras.route('/obras/<int:id>', methods=['PUT'])
-def modificar(id):
-    obra_schema = ObraSchema(many=True)
-    autor_schema = AutorSchema(many=True)
+def editar(id):
+    autor_schema = AutorSchema()
+    obra_schema = ObraSchema()
 
     try:
-        obra = Obra.query.filter(Obra.id == id).one()
+        query_obra = Obra.query.filter(Obra.id == id).one()
     except NoResultFound:
         return jsonify({'mensagem': 'Obra não encontrada.'}), 400
 
-    obra_result = obra_schema.dump(obra)
-    autor_result = autor_schema.dump(obra.quotes.all())
-    return jsonify({'obra': obra_result, 'autores': autor_result}), 200
+    json_data = request.json
+    if not json_data:
+        return jsonify({'messagem': 'Nenhum dado de entrada fornecido'}), 400
+
+    data_autores = dict(nomes=json_data['autores'])
+    del json_data['autores']
+
+    try:
+        data_obra = obra_schema.load(json_data, instance=query_obra)
+    except ValidationError as err:
+        return err.messages, 422
+    
+    # for data_autor in data_autores['nomes']:
+    #     try:
+    #         data_autor = autor_schema.load(dict(nome=data_autor))
+    #     except ValidationError as err:
+    #         return err.messages, 422
+
+    #     autor = Autor.query.filter_by(nome=data_autor.nome).first()
+    #     if autor:
+    #         return jsonify({'messagem': f'Autor já cadastrado com esse nome "{data_autor.nome}"'}), 422
+
+    #     autor = Autor(nome=data_autor.nome, obra_id=data_obra.id)
+    #     current_app.db.session.add(autor)
+
+    current_app.db.session.commit()
+
+    obra_result = obra_schema.dump(Obra.query.get(data_obra.id))
+
+    return obra_schema.jsonify(obra_result), 201
 
 
 @bp_obras.route('/obras/<int:id>', methods=['DELETE'])
